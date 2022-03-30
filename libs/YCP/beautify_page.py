520254576e343937443abf0ae19e547f3ae53e30
Bill Zhong <bill_zhong@perfectcorp.com>
2022-03-30 18:26:35 +0800

    Add some pages

---
+++ libs/YCP/beautify_page.py
@@ -0,0 +1,21 @@
+class BeautifyPage:
+    def __init__(self) -> None:
+        pass
+
+    def click_back(self):
+        pass
+
+    def click_save(self):
+        pass
+
+    def click_effects(self):
+        pass
+
+    def click_smooth(self):
+        pass
+
+    def click_face_shaper(self):
+        pass
+
+    def click_body_tuner(self):
+        pass
---
+++ libs/YCP/photo_pick_page.py
@@ -0,0 +1,9 @@
+class PhotoPickPage:
+    def __init__(self) -> None:
+        pass
+
+    def select_album(self, num):
+        pass
+
+    def select_photo(self, num):
+        pass
